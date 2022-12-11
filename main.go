e4d57cffe44ce92cdad93a55e8ab1be536ac850f
Marshall Schmitz <mschmitz08@alumni.uwosh.edu>
2022-12-11 01:56:47 -0600

    removed album references added receipt references

--- main.go
+++ main.go
@@ -7,65 +7,58 @@ import (
 )
 
 // receipts
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type receipt struct {
+	retailer     string          `json:"retailer"`
+	purchaseDate string          `json:"purchaseDate"`
+	purchaseTime string          `json:"purchaseTime"`
+	items        []receipt_items `json:"items"`
+	total        string          `json:"total"`
 }
 
 // receipt items
 type receipt_items struct {
+	shortDescription string `json:"shortDescription"`
+	price            string `json:"price"`
 }
 
 // albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+var receipts = []receipt{
+	{retailer: "test", purchaseDate: "2022-01-01", purchaseTime: "13:01", total: "10", []receipt_items{shortDescipriton: "someitem", price: "10"}}
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+	router.GET("/receipts/:id/points", getReceiptByID)
+	router.POST("/receipts/process", postReceipts)
 
 	router.Run("localhost:8080")
 }
+// postRecept adds an receipt from JSON received in the request body.
+func postReceipts(c *gin.Context) {
+    var newReceipt receipt
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+    // Call BindJSON to bind the received JSON to
+    // newReceipt.
+    if err := c.BindJSON(&newReceipt); err != nil {
+        return
+    }
+
+    // Add the new receipt to the slice.
+    receipts = append(receipts, newReceipts)
+    c.IndentedJSON(http.StatusCreated, newReceipt)
 }
-
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
+// getReceiptByID locates the receipt whose ID value matches the id
+// parameter sent by the client, then returns that receipt as a response.
+func getReceiptByID(c *gin.Context) {
 	id := c.Param("id")
 
-	// Loop through the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
+	// Loop through the list of receipts, looking for
+	// an receipts whose ID value matches the parameter.
+	for _, a := range receipts {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
 			return
 		}
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "receipt not found"})
 }
