aebcc35a70422e32c7b27bafe5a127a67d719469
Marshall Schmitz <mschmitz08@alumni.uwosh.edu>
2022-12-11 02:41:49 -0600

    added map, corrected compiler errors

--- main.go
+++ main.go
@@ -4,61 +4,67 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 // receipts
 type receipt struct {
-	retailer     string          `json:"retailer"`
-	purchaseDate string          `json:"purchaseDate"`
-	purchaseTime string          `json:"purchaseTime"`
-	items        []receipt_items `json:"items"`
-	total        string          `json:"total"`
+	Retailer     string          `json:"retailer"`
+	PurchaseDate string          `json:"purchaseDate"`
+	PurchaseTime string          `json:"purchaseTime"`
+	Items        []receipt_items `json:"items"`
+	Total        string          `json:"total"`
+	points       string
 }
 
 // receipt items
 type receipt_items struct {
-	shortDescription string `json:"shortDescription"`
-	price            string `json:"price"`
+	ShortDescription string `json:"shortDescription"`
+	Price            string `json:"price"`
 }
 
 // albums slice to seed record album data.
-var receipts = []receipt{
-	{retailer: "test", purchaseDate: "2022-01-01", purchaseTime: "13:01", total: "10", []receipt_items{shortDescipriton: "someitem", price: "10"}}
-}
+//
+//	var receipts = []receipt{
+//		{id: "someidhere", retailer: "test", purchaseDate: "2022-01-01", purchaseTime: "13:01", total: "10", items: []receipt_items{{shortDescription: "someitem", price: "10"}}},
+//	}
+var receipts map[string]receipt
 
 func main() {
+	receipts = make(map[string]receipt)
+	receipts["someidhere"] = receipt{
+		Retailer: "test", PurchaseDate: "2022-01-01", PurchaseTime: "13:01", Total: "10", Items: []receipt_items{{ShortDescription: "someitem", Price: "10"}},
+	}
 	router := gin.Default()
 	router.GET("/receipts/:id/points", getReceiptByID)
 	router.POST("/receipts/process", postReceipts)
 
 	router.Run("localhost:8080")
 }
+
 // postRecept adds an receipt from JSON received in the request body.
 func postReceipts(c *gin.Context) {
-    var newReceipt receipt
-
-    // Call BindJSON to bind the received JSON to
-    // newReceipt.
-    if err := c.BindJSON(&newReceipt); err != nil {
-        return
-    }
-
-    // Add the new receipt to the slice.
-    receipts = append(receipts, newReceipts)
-    c.IndentedJSON(http.StatusCreated, newReceipt)
+	var newReceipt receipt
+	uuidWithHyphen := uuid.New()
+	// Call BindJSON to bind the received JSON to
+	// newReceipt.
+	if err := c.BindJSON(&newReceipt); err != nil {
+		return
+	}
+	receipts[uuidWithHyphen.String()] = newReceipt
+	c.IndentedJSON(http.StatusCreated, newReceipt)
 }
+
 // getReceiptByID locates the receipt whose ID value matches the id
 // parameter sent by the client, then returns that receipt as a response.
 func getReceiptByID(c *gin.Context) {
 	id := c.Param("id")
 
-	// Loop through the list of receipts, looking for
-	// an receipts whose ID value matches the parameter.
-	for _, a := range receipts {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+	val, ok := receipts[id]
+	// If the key exists
+	if ok {
+		c.IndentedJSON(http.StatusOK, val)
+		return
 	}
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "receipt not found"})
 }
